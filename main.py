9b9e7e5ce66f450a1c8a663c919f9c075b849ccb
t-shinkai00 <t.shinkai00@gmail.com>
2022-10-19 18:44:00 +0900

    add sleep

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from get_ig_urls import get_ig_urls
 from search_ig import search_ig
 
+import time
+
 
 def main():
   # 検索
   key = '"#銀座" "#髪質改善"'
   ig_urls = get_ig_urls(key)
   for i, url in enumerate(ig_urls):
-    print(i, url)
+    # print(i, url)
+    time.sleep(2)
+    search_ig(url)
 
 if __name__ == '__main__':
   main()
