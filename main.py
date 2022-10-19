ab154be26b751e5be702d0d3307964a5d2ab3b3e
t-shinkai00 <t.shinkai00@gmail.com>
2022-10-19 18:21:18 +0900

    get instagram urls

---
+++ main.py
@@ -0,0 +1,11 @@
+from get_ig_urls import get_ig_urls
+
+def main():
+  # 検索
+  key = '"#銀座" "#髪質改善"'
+  ig_urls = get_ig_urls(key)
+  for i, url in enumerate(ig_urls):
+    print(i, url)
+
+if __name__ == '__main__':
+  main()
